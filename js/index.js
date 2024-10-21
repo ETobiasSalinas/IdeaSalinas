018b0b6bca090b8a52b2ca33713b026818761711
tobias salinas <tobiassalinas02@gmail.com>
2024-10-21 19:22:20 -0300

    segunda pre entrega de javascript

--- js/index.js
+++ js/index.js
@@ -26,6 +26,87 @@ function comprar() {
 
 }
 
-comprar();
+comprar(); 
+
+let remeras = [
+    {
+        remera: "simple",
+        color: "todos",
+        talle: "l al xl",
+        precio: 2999.99
+    },
+    {
+        remera: "oversize",
+        color: "todos",
+        talle: "unico",
+        precio: 4999.99
+    },
+    {
+        remera: "estampado",
+        color: "todos",
+        talle: "l al xl",
+        precio: 3499.99
+    },
+    {
+        remera: "termica",
+        color: "negro",
+        talle: "m al l",
+        precio: 5999.99
+    }
+]
+
+for(let i=0; i<remeras.length; i++){
+    console.log(remeras[i])
+}
+
+preciosRedondeados = remeras.map (precios => Math.ceil(precios.precio));
+console.log(preciosRedondeados)
+
+let pantalones = [
+    {
+        pantalon: "jogin",
+        color: "gris, negro",
+        talle: "l al xl",
+        precio: 1500
+    },
+    {
+        pantalon: "jean",
+        color: "todos",
+        talle: "l al xl",
+        precio: 3000
+    },
+    {
+        pantalon: "cargo",
+        color: "sin stock",
+        talle: "sin stock",
+        precio: 2500
+    },
+]
+
+let prendasDeVestir = remeras.concat(pantalones)
+console.log(prendasDeVestir)
+
+let totalRemeras = remeras.reduce((total, remera) => total + remera.precio, 0);
+let totalPantalones = pantalones.reduce((total, pantalon) => total + pantalon.precio, 0);
+let precioTotal = totalRemeras + totalPantalones;
+let precioTotalRedondeado = Math.ceil(precioTotal)
+
+console.log(`El precio total de las remeras es $${totalRemeras}`);
+console.log(`El precio total de los pantalones es $${totalPantalones}`);
+console.log(`El precio total de remeras y pantalones es $${precioTotalRedondeado}`);
+
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
 
 
